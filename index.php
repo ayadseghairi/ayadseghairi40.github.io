5e23bb69edef97e0b47c8db402a640678aa6ca1b
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2023-04-02 15:05:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<title>Redirecting to a new page...</title>
 	<meta http-equiv="refresh" content="5; URL=https://ayadseghairi.github.io/Personal">
 </head>
-//<body>
-	//<p>You are being automatically redirected to our new website...</p>
-	//<p>If you are not redirected within 5 seconds, please click <a href="https://ayadseghairi.github.io/Personal">here</a>.</p>
+<body>
+	<p>You are being automatically redirected to our new website...</p>
+	<p>If you are not redirected within 5 seconds, please click <a href="https://ayadseghairi.github.io/Personal">here</a>.</p>
 </body>
 </html>
