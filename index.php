aaf15e82b70ee4966624d5e7b6eafe9ed18a805d
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2023-04-02 15:02:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Redirecting to a new page...</title>
+	<meta http-equiv="refresh" content="5; URL=https://ayadseghairi.github.io/Personal">
+</head>
+//<body>
+	//<p>You are being automatically redirected to our new website...</p>
+	//<p>If you are not redirected within 5 seconds, please click <a href="https://ayadseghairi.github.io/Personal">here</a>.</p>
+</body>
+</html>
